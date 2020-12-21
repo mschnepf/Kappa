301263e175320827801745077479e8a639ab3d52
Matthias Schnepf <matthias.schnepf@kit.edu>
2020-12-21 09:53:09 +0100

    Add skimming configs for QCD 2016

---
+++ Skimming/zjet/configs/qcd16/106X_SingleMuon_Run2016-21Feb2020_UL2016_HIPM-v1/testKappaSkim.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+_GT="106X_dataRun2_v27"
+_NEVT=1000
+_IS_DATA=True
+_GRID_PATH_PREFIX="root://xrootd-cms.infn.it/"
+_FILE="/store/data/Run2016F/SingleMuon/MINIAOD/21Feb2020_UL2016_HIPM-v1/10000/9673982A-87D1-5749-964E-742BC30CB570.root"
+_DIR="test/${_GT}"
+
+mkdir -p $_DIR
+
+echo "##############################################################################"
+echo "STARTING cmsRun !!!!"
+echo "##############################################################################"
+
+cmsRun ../kappaSkim_default_cfg.py inputFiles=${_GRID_PATH_PREFIX}${_FILE} \
+                        globalTag=${_GT} \
+                        isData=${_IS_DATA} \
+                        outputFile=testKappaSkim_out_${_GT}.root \
+                        maxEvents=${_NEVT} \
+                        dumpPython=1 2>&1 | tee cout_${_GT}.log
+
+echo "##############################################################################"
+echo "cmsRun terminated !!!!"
+echo "##############################################################################"
+
+if [ -f cout_$_GT.log ]; then
+    mv cout_$_GT.log $_DIR/
+fi
+
+if [ -f infos.log ]; then
+    mv infos.log $_DIR/
+fi
+
+if [ -f debugs.log ]; then
+    mv debugs.log $_DIR/
+fi
+
+if [ -f testKappaSkim_out_${_GT}_numEvent${_NEVT}.root ]; then
+    mv testKappaSkim_out_${_GT}_numEvent${_NEVT}.root $_DIR/
+fi
